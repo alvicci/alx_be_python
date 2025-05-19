e36a6b20fee5c2fc8620fc12cc75aabdb43976f8
Olaitan Adedokun <kenzdex@gmail.com>
2025-05-19 15:20:16 +0100

    Python script that asks the user for their current age and then calculates how old they will be in a specific future year

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# A python script that asks the user for their current age
+# and then calculates how old they will be in a specific future year
+
+current_age = int(input("How old are you? "))
+
+age = current_age + 27
+
+print(f"In 2050, you will be {age} years old.")
