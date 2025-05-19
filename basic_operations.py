4fcc7f4604de29bc60543c765cbd9560ddbf678b
Olaitan Adedokun <kenzdex@gmail.com>
2025-05-19 14:56:24 +0100

    script performs basic arithmetic operations with two predefined numbers

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+# This script performs basic arithmetic operations with two predefined numbers.
+
+number1 = 10
+number2 = 5
+
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
