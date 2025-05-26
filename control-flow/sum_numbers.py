510a4c957303b228c17fc8bdb74bc7fcba050b79
Olaitan Adedokun <kenzdex@gmail.com>
2025-05-26 10:38:43 +0100

    Python program using a for loop to calculate the sum of all the numbers in a list.

---
+++ control-flow/sum_numbers.py
@@ -0,0 +1,10 @@
+# A Python program using a for loop to calculate the sum
+# of all the numbers in a list.
+
+numbers = [1, 5, 3, 9]
+total = 0
+
+for number in numbers:
+    total += number
+
+print(total)
