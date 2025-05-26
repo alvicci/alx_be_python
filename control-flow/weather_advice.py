de41f507a6d34d60b841f4f97f648a36bfd80631
Olaitan Adedokun <kenzdex@gmail.com>
2025-05-26 08:22:21 +0100

    remove the space from the prompt

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,13 @@
 # A script will ask the user about the current weather conditions
 # and provide clothing recommendations based on the input
 
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+current_weather = input("What's the weather like today? (sunny/rainy/cold):")
 
 if current_weather == "sunny":
     print(f"Wear a t-shirt and sunglasses.")
 elif current_weather == "rainy":
-    print("Don't forget your umbrella and a raincoat.")
+    print(f"Don't forget your umbrella and a raincoat.")
 elif current_weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.")
+    print(f"Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don't have recommendations for this weather.")
+    print(f"Sorry, I don't have recommendations for this weather.")
