9a3d64ff38aec226df2a107eb834b5d62cd89c5d
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-03 14:42:55 +0100

    A function that performs basic arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+# A function that performs basic arithmetic operations
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num1 == 0 or num2 == 0:
+                return "Cannot divide by zero."
+            return num1 / num2
