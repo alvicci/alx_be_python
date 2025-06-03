cdbae2b16f042e979cb0b24d8ddeb4603d66e8e8
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-03 14:48:32 +0100

    Changed the implementation to use if elif instead of match statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,12 @@
 # A function that performs basic arithmetic operations
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num1 == 0 or num2 == 0:
-                return "Cannot divide by zero."
-            return num1 / num2
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num1 == 0 or num2 == 0:
+            return "Cannot divide by zero."
+        return num1 / num2
