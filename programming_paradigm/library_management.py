2ff6454fb18347aeb092f735e7694a3b715ca903
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-09 22:42:11 +0100

    Implement a Book and Library classes for the library_managment script

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,48 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    @property
+    def is_checked_out(self):
+        return self._is_checked_out
+
+    @is_checked_out.setter
+    def is_checked_out(self, is_checked_out):
+        self._is_checked_out = is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._book = []
+
+    def add_book(self, book):
+        new_book = {
+            "title": book.title,
+            "author": book.author,
+            "checkout": book.is_checked_out
+        }
+        self._book.append(new_book)
+
+    def check_out_book(self, title):
+        for item in self._book:
+            if item["title"] == title:
+                item["checkout"] = True
+
+    def return_book(self, title):
+        for item in self._book:
+            if item["title"] == title:
+                item["checkout"] = False
+
+    def list_available_books(self):
+        for item in self._book:
+            if not item["checkout"]:
+                print(f"{item["title"]} by {item["author"]}")
+
+# Test code
+# l = Library()
+# l.add_book(Book("Brave New World", "Aldous Huxley"))
+# l.add_book(Book("1984", "George Orwell"))
+# l.check_out_book("1984")
+# l.list_available_books()
