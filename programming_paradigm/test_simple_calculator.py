15843e07adcd305cde56eda1ea802458bc2f20ce
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-11 13:19:18 +0100

    added more test cases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,21 +11,40 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-    
+        self.assertEqual(self.calc.add(-2, -3), -5)
+        self.assertEqual(self.calc.add(-3, 1), -2)
+        self.assertEqual(self.calc.add(2.5, 3.1), 5.6)
+        with self.assertRaises(TypeError):
+            self.calc.add("2", 3)
+
     def test_subtraction(self):
         """Test the subtract method."""
         self.assertEqual(self.calc.subtract(3, 3), 0)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
-    
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-2, -3), 1)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(5.5, 2.2), 3.3)
+        with self.assertRaises(TypeError):
+            self.calc.subtract("5", 2)
+
     def test_multiplication(self):
         """Test the multiply method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
-    
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-4, 5), -20)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(2.5, 4), 10.0)
+        self.assertEqual(self.calc.multiply("3", 2), "33")
+
     def test_division(self):
         """Test the divide method."""
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertIsNone(self.calc.divide(10, 0), None)
-        # with self.assertRaises(ZeroDivisionError):
-        #     self.calc.divide(10, 0)
+        self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333, places=4)
+        with self.assertRaises(TypeError):
+            self.calc.divide("10", 2)
+        self.assertEqual(self.calc.divide(-10, 2), -5.0)
+        self.assertEqual(self.calc.divide(0, 5), 0.0)
