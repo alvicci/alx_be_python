5ba46316b548e06a7c876ce533c5d91c1003d62d
Olaitan Kehinde <105742731+jackOfAllNames@users.noreply.github.com>
2025-06-19 16:34:24 +0100

    remove the protection of the attributes

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,31 +8,15 @@
 """
 class Book:
     def __init__(self, title: str, author: str, year: int):
-        self.__title = title
-        self.__author = author
-        self.__year = year
+        self.title = title
+        self.author = author
+        self.year = year
 
     def __str__(self):
-        return f"{self.__title} by {self.__author}, published in {self.__year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book('{self.__title}', '{self.__author}', {self.__year})"
-
-    @property
-    def title(self):
-        return self.__title
-
-    @property
-    def author(self):
-        return self.__author
-
-    # @author.setter
-    # def author(self, value):
-    #     self.__author = value
-
-    @property
-    def year(self):
-        return self.__year
+        return f"Book('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
-        print(f"Deleting {self.__title}")
+        print(f"Deleting {self.title}")
