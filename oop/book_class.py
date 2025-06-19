2fe8275a37d24ff5b073e3b9b9d86eadba45d056
Olaitan Kehinde <105742731+jackOfAllNames@users.noreply.github.com>
2025-06-19 16:01:44 +0100

    The file with a short description as the script docstring

---
+++ oop/book_class.py
@@ -0,0 +1,8 @@
+"""
+    Objective:
+    Master Python magic methods by implementing a Book class that demonstrates:
+    - Constructor (__init__)
+    - Destructor (__del__)
+    - String representation (__str__)
+    - Official representation (__repr__)
+"""
