146a888335afff8587e8c3ddafc08ae34a36c2fa
Olaitan Kehinde <105742731+jackOfAllNames@users.noreply.github.com>
2025-06-19 16:14:02 +0100

    created a read only attributes

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,3 +6,27 @@
     - String representation (__str__)
     - Official representation (__repr__)
 """
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        self.__title = title
+        self.__author = author
+        self.__year = year
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def __author(self):
+        return self.__author
+
+    @property
+    def __year(self):
+        return self.__year
+
+    def __del__(self):
+        print(f"Deleting (title of the book)") 
+
+# title (str): The title of the book.
+# author (str): The author of the book.
+# year (int): The publication year of the book.
