2360adf12835108dcd3b722c0242f2aa30a016d9
Olaitan Kehinde <105742731+jackOfAllNames@users.noreply.github.com>
2025-06-19 17:48:17 +0100

    created the derive and base classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,2 +1,16 @@
 # Objective: Showcase inheritance with Book, EBook, PrintBook classes
 #   and composition with a Library class managing books.
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
