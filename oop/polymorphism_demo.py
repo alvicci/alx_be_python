26d6aa0c1ef7cc5a884385d28cf3169a1c45e553
Olaitan Adedokun <kenzdex@gmail.com>
2025-06-20 13:21:22 +0100

    created the polymorphism script

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,37 @@
+import math
+"""
+Objective:
+    Enhance your understanding of polymorphism in Python by
+    creating a set of classes that demonstrate method
+    overriding and polymorphic behavior.
+
+Task Description:
+    Create a Python script named polymorphism_demo.py. In this script:
+    - Define a base class `Shape` with a method `area()`.
+    - Create two derived classes: `Rectangle` and `Circle`, each overriding
+        the `area()` method to compute their respective areas.
+    - Demonstrate polymorphism by calling the `area()` method on different
+        shape objects through a common interface.
+"""
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return (self.length * self.width)
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        return (math.pi * (self.radius ** 2))
